dd7da1d6afee0774d2b42eb653b90fd9c133a24d
Zig Hooker <zighooker@gmail.com>
2023-09-03 14:44:41 -0700

    Method completion

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,16 +1,24 @@
-def substrings(input_word, dictionary)
+def substrings(input_word, input_dictionary)
   input_array = input_word.split("")
-  output_array = []
+  test_array = []
+  result_hash = Hash.new(0)
 
   input_array.each_with_index do |letter, index|
-    test_array = input_array.slice(index..-1)
-
-    test_array.each_with_index do |subletter, subindex|
-      puts "#{letter}#{subletter}"
+    temp = ""
+    for sub_index in index..input_array.length - 1 do
+      temp += input_array[sub_index].to_s
+      test_array.push(temp)
     end
-    # puts test_array.join("")
   end
+
+  test_array.each do |test_item|
+    if input_dictionary.include?(test_item)
+      result_hash[test_item] += 1
+    end
+  end
+  result_hash
 
 end
 
-substrings("test", 3)
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
